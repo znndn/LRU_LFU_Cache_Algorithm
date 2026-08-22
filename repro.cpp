6805364d1cf7ea9989c039c6f0321c32c9278850
zwd <1712332665@qq.com>
2026-08-23 03:02:01 +0800

    提交repro

--- repro.cpp
+++ repro.cpp
@@ -1,13 +1,12 @@
-// repro.cpp
 #include "LFUAlgorithm.h"
 #include <string>
 
 int main()
 {
-    for (int i = 0; i < 100; ++i)   // 循环 100 次放大泄漏
+    for (int i = 0; i < 100; ++i)  
     {
-        LFU::LFUAlgorithm<int, std::string> c(1);  // 阈值 = 1
-        c.put("a", 1);                             // value="a", key=1
+        LFU::LFUAlgorithm<int, std::string> c(1);  
+        c.put("a", 1);                           
         std::string out;
         c.get(1, out);
     }
